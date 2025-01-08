16ef417e8f001881436d4755d7ddc627582caffa
Maria Zhiganova <my.nik.mariann@gmail.com>
2025-01-08 16:32:35 +0300

    adding functions to read and get info from files

---
+++ src/utils.py
@@ -0,0 +1,66 @@
+import json
+from typing import List
+
+import pandas as pd
+
+from settings import EXCEL_PATH
+
+
+def get_xlsx(file_path: str) -> list[dict]:
+    """
+    Функция, принимающая путь до Excel-файла и возвращающая список словарей.
+    """
+    try:
+
+        df = pd.read_excel(file_path)
+        if isinstance(df, pd.DataFrame) and not df.empty:
+            return df.to_dict(orient='records')
+
+        else:
+            return []
+
+    except FileNotFoundError:
+        return []
+
+
+def get_json_currencies(file_path: str) -> List[str]:
+    """
+    Функция, принимающая путь к JSON файлу и возвращающая список данных из файла.
+    """
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+            if "user_currencies" in data:
+                result = data["user_currencies"]
+
+        return result
+
+
+    except json.JSONDecodeError as ex:
+        raise ValueError(f"Ошибка при чтении файла: {ex}")
+
+    except Exception as ex:
+        raise Exception(f"Ошибка при чтении файла: {ex}")
+
+
+def get_json_stocks(file_path: str) -> List[str]:
+    """
+    Функция, принимающая путь к JSON файлу и возвращающая список данных из файла.
+    """
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+            if "user_stocks" in data:
+                result = data["user_stocks"]
+
+            return result
+
+
+    except json.JSONDecodeError as ex:
+        raise ValueError(f"Ошибка при чтении файла: {ex}")
+
+
+    except Exception as ex:
+        raise Exception(f"Ошибка при чтении файла: {ex}")
