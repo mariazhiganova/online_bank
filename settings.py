7f77323afc5f4df2f2b4fc179dd6cc8cc7b4405e
Maria Zhiganova <my.nik.mariann@gmail.com>
2025-01-12 11:45:51 +0300

    adding new path

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ BASE_DIR = Path(__file__).resolve().parent
 EXCEL_PATH = BASE_DIR.joinpath('data/my_operations.xls')
 JSON_PATH = BASE_DIR.joinpath('user_settings.json')
 LOG_PATH = BASE_DIR.joinpath('logs/app.log')
+REPORTS_PATH = BASE_DIR.joinpath('reports.json')
